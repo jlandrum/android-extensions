f079cabf7970cb25a549f17c4ffab0741376c78d
James Landrum <thyme.cypher@gmail.com>
2017-05-30 10:54:45 -0400

    .

--- src/main/kotlin/com/jameslandrum/axt/view.kt
+++ src/main/kotlin/com/jameslandrum/axt/view.kt
@@ -2,5 +2,5 @@ package com.jameslandrum.axt
 
 import android.view.View
 
-infix fun View.showIf(condition:Boolean) { this.visibility = if (condition) View.VISIBLE else View.GONE }
-infix fun View.hideIf(condition:Boolean) { this.visibility = if (!condition) View.VISIBLE else View.GONE }
+infix fun View.showIf(condition:Boolean?) { this.visibility = if (condition?:false) View.VISIBLE else View.GONE }
+infix fun View.hideIf(condition:Boolean?) { this.visibility = if (!(condition?:false)) View.VISIBLE else View.GONE }
