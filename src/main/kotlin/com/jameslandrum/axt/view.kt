3fadc7c41f414aad927e1085fd69a89cce538e15
James Landrum <thyme.cypher@gmail.com>
2017-05-31 15:53:17 -0400

    add disable/enableif

--- src/main/kotlin/com/jameslandrum/axt/view.kt
+++ src/main/kotlin/com/jameslandrum/axt/view.kt
@@ -4,3 +4,5 @@ import android.view.View
 
 infix fun View.showIf(condition:Boolean?) { this.visibility = if (condition?:false) View.VISIBLE else View.GONE }
 infix fun View.hideIf(condition:Boolean?) { this.visibility = if (!(condition?:false)) View.VISIBLE else View.GONE }
+infix fun View.enableIf(condition:Boolean?) { this.isEnabled = condition?:false }
+infix fun View.disableIf(condition:Boolean?) { this.isEnabled = !(condition?:false) }
